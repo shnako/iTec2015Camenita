253f5dd8f9fee4f09ec5a4761c1e9ce9b2be7005
Emanuel Danci <danci.emanuel@gmail.com>
2015-04-04 00:43:25 +0300

    Fix the API urls

--- unified_test/UnifiedTest/urls.py
+++ unified_test/UnifiedTest/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, url
 
 from UnifiedTest import views
-from UnifiedTest.api import views as api_views
+from UnifiedTest.api import UsePage
 
 urlpatterns = patterns('',
      url(r'^$', views.index, name='index'),
      url(r'^create/$', views.create_page, name='create-page'),
      url(r'^view/(?P<page_ref>.*)$', views.view_page_details, name='view-page'),
-     url(r'^use/(?P<page_ref>.*)$', api_views.UsePage.as_view(), name='use-page'),
+     url(r'^use/(?P<page_ref>.*)$', UsePage.as_view(), name='use-page'),
 )
