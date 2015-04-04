bcf258a6e81ed538f2f554d78a2094f88517108d
Vlad Iulian Schnakovszki <shnako@yahoo.com>
2015-04-04 12:04:24 +0300

    Modified wsgi file to work with apache

--- unified_test/unified_test/wsgi.py
+++ unified_test/unified_test/wsgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unified_test.settings")
+sys.path.append('/projects/iTec2015Camenita/unified_test/unified_test')
+sys.path.append('/projects/iTec2015Camenita/unified_test')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
